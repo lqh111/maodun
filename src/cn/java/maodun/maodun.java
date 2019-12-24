1b3a91d42c9d4cc1db63c686a5445e78af98ff9f
lqh111 <2718852535@qq.com>
2019-12-24 10:36:29 +0800

    maodun

---
+++ src/cn/java/maodun/maodun.java
@@ -0,0 +1,13 @@
+package cn.java.maodun;
+
+public class maodun {
+private int maodun;
+
+public int getMaodun() {
+	return maodun;
+}
+
+public void setMaodun(int maodun) {
+	this.maodun = maodun;
+}
+}
